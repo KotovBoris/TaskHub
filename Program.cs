4cbd45e728fc242d099488c0c26cc25ecfa2201e
Kotov Boris <b.kotov@vkteam.ru>
2026-05-21 18:30:00 +0300

    init: создан проект TaskHub (dotnet console)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using TaskHub.Services;
+using TaskHub.UI;
+
+namespace TaskHub;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string filePath = "tasks.json";
+
+        using var fileService = new FileService(filePath);
+        var taskManager = new TaskManager();
+        using var deadlineChecker = new DeadlineChecker(taskManager, TimeSpan.FromSeconds(15));
+
+        var ui = new ConsoleUI(taskManager, fileService, deadlineChecker);
+        await ui.RunAsync();
+    }
+}
